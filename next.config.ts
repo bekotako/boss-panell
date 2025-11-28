16929353b5df5b98b4e98346305dcf4da8aaff66
Berkay Ogdey <bekotako01@gmail.com>
2025-11-29 00:46:24 +0300

    fix build config webpack issue

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,21 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Sadece yayında çalışsın
+  disable: process.env.NODE_ENV === "development", // Geliştirme modunda PWA kapalı
+  buildExcludes: [/middleware-manifest.json$/], // Hata çıkaran dosyayı hariç tut
 });
 
 const nextConfig: NextConfig = {
+  // TypeScript ve ESLint hatalarını görmezden gel (Yayınlamak için)
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  // Resim optimizasyonu hatasını önle
+  images: {
+    unoptimized: true,
   },
 };
 
